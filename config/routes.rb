644d0e2f3510306becc39978fa3a46265d496852
choko0916 <n.yuka0702@gmail.com>
2022-12-30 16:26:50 +0900

    #4 [add] http://localhost:3000/ にアクセスした際に表示されるページを指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "homes#index"
+  get "homes/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "welcome#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
