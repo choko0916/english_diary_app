deba8573d6b7a19f932d4df2ee98200b0f32695a
choko0916 <n.yuka0702@gmail.com>
2022-12-31 00:53:05 +0900

    #4 [fix] typo修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "homes#index"
   get "homes/show"
   devise_for :users, controllers: {
-    registrations: "users/registration",
+    registrations: "users/registrations",
     sessions: "users/sessions"
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
