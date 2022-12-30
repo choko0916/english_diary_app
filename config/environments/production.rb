31ea7fd5a741394581db93bcb0cda81bd7e962d0
choko0916 <n.yuka0702@gmail.com>
2022-12-30 19:31:15 +0900

    #4 [update] メール送信用サーバー（本番環境）を設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: "https://polar-citadel-36477.herokuapp.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :domain => "heroku.com",
+    :user_name => "apikey",
+    :password => ENV["SENDGRID_API_KEY"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
