7d339656aa8b055b97948ce0df0e3ef4c16e4de6
choko0916 <n.yuka0702@gmail.com>
2022-12-30 16:29:07 +0900

    #4 [add] Homesコントローラーを追加

---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/homes/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/homes/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
