3f3e7bcd84a3f227425e8101722a6d16be9a7d81
choko0916 <n.yuka0702@gmail.com>
2022-12-30 16:38:38 +0900

    #4 [add] Userモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "homes#index"
   get "homes/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
