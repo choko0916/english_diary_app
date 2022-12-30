62820a6cd92ef8f8b77208937dee0e9052312624
choko0916 <n.yuka0702@gmail.com>
2022-12-30 19:23:10 +0900

    #4 [update] メール送信用サーバー（開発環境）を設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :domain => 'localhost',
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -67,7 +77,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # mailer setting
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
