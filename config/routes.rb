677692283afbc77eed75b194d507536d35f1fbe4
choko0916 <n.yuka0702@gmail.com>
2022-12-31 00:44:48 +0900

    #4 [update] Rails7におけるsign_up時のエラーが表示されない問題への対処

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "homes#index"
   get "homes/show"
+  devise_for :users, controllers: {
+    registrations: "users/registration"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
