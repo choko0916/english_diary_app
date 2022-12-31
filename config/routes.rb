223463bfb5fcba23f0d18f5fd256ea3aff1835ae
choko0916 <n.yuka0702@gmail.com>
2022-12-31 16:33:39 +0900

    #4 [update] Rails7におけるafter_sign_out_path_forが効かない問題、るsign_up時のエラーが表示されない問題への対処

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "homes#index"
   get "homes/show"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
