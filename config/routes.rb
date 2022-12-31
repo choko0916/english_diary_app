55099ef8d0936a139260bf7804237f5876358e5c
choko0916 <n.yuka0702@gmail.com>
2022-12-31 16:22:57 +0900

    #4 [fix]  Invalid route name, already in use: new_user_sessionというエラー解消のため一旦削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root "homes#index"
   get "homes/show"
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: "users/sessions"
-  }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
