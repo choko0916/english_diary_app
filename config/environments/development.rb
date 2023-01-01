bdc960c1e52c70413bcdcb0fff42d0b444808aad
choko0916 <n.yuka0702@gmail.com>
2023-01-01 17:27:31 +0900

    #4 [fix] シングルクオートをダブルクオートに修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,11 +42,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+    :address => "smtp.sendgrid.net",
     :port => 587,
-    :domain => 'localhost',
-    :user_name => 'apikey',
-    :password => ENV['SENDGRID_API_KEY'],
+    :domain => "localhost",
+    :user_name => "apikey",
+    :password => ENV["SENDGRID_API_KEY"],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
