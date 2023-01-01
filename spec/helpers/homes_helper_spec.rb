6321c23bc36f289a2c9056d8b20666f2253011fb
choko0916 <n.yuka0702@gmail.com>
2023-01-01 23:39:53 +0900

    #4 rubocopでの指摘箇所を修正

--- spec/helpers/homes_helper_spec.rb
+++ spec/helpers/homes_helper_spec.rb
@@ -10,6 +10,6 @@ require "rails_helper"
 #     end
 #   end
 # end
-RSpec.describe HomesHelper, type: :helper do
+RSpec.describe HomesHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe "Homes" do
   describe "GET /index" do
     it "returns http success" do
       get "/homes/index"
