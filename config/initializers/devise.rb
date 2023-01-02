c4a2af1868e3f63a1e0325173325a03d1dd0643b
choko0916 <n.yuka0702@gmail.com>
2023-01-02 13:04:12 +0900

    #4 [update] アカウントの認証を５回間違うと、アカウントをロックするようにする機能を追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -203,11 +203,11 @@ Devise.setup do |config|
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  # config.unlock_strategy = :both
+  config.unlock_strategy = :email
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  # config.maximum_attempts = 20
+  config.maximum_attempts = 4
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
