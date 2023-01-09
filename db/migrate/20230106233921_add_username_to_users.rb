ef9fbf3b5b1a143596f600a6bcd4833acd31d534
choko0916 <n.yuka0702@gmail.com>
2023-01-09 21:57:22 +0900

    #4 [fix] 必要なデフォルト値を設定

--- db/migrate/20230106233921_add_username_to_users.rb
+++ db/migrate/20230106233921_add_username_to_users.rb
@@ -1,5 +1,5 @@
 class AddUsernameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :username, :string, limit: 50, null: false
+    add_column :users, :username, :string, limit: 50, null: false, default: "名無し"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_233921) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", limit: 50, null: false
+    t.string "username", limit: 50, default: "名無し", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
