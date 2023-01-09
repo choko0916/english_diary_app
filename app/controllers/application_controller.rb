0cf7cb9547093323963d2b0fde703128f9d61329
choko0916 <n.yuka0702@gmail.com>
2023-01-09 20:48:58 +0900

    #4 [fix] 行のずれを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 
   private
 
