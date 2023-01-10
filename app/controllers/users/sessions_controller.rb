dcbcf9e39249103eb20c1c96b065ae917f50a624
choko0916 <n.yuka0702@gmail.com>
2023-01-11 01:59:24 +0900

    #6 [fix] noticeの後に必要な:を追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,7 +28,7 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice "ゲストユーザーとしてログインしました"
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました"
   end
 
   # protected
