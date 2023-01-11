3b72217e639973a1120510d803d5a0db1be78dc9
choko0916 <n.yuka0702@gmail.com>
2023-01-12 01:58:28 +0900

    #6 [update] config/application.rbに設定してあるタイムゾーンを元に現在日時を取得するよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.confirmed_at = Time.zone.now
+      user.confirmed_at = Time.current
       user.username = "ゲスト"
     end
   end
