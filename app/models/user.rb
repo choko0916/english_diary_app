62ae22e9e690fc79545376d9cdad339756a0e9b1
choko0916 <n.yuka0702@gmail.com>
2023-01-15 16:34:30 +0900

    #8 [update] 画像の有効なフォーマットと、アップロードサイズの上限を指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
+  validates :avatar, content_type: { in: %w[image/jpeg image/gif image/png],
+                                     message: "有効なフォーマットではありません" },
+                     size: { less_than: 5.megabytes, message: " 5MBを超える画像はアップロードできません" }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
