4ab096f3f3c1800b0873b93ae370d876e395a306
choko0916 <n.yuka0702@gmail.com>
2023-01-22 20:48:04 +0900

    #11 [fix] 抜けていた:を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :diaries, dependent: destroy
+  has_many :diaries, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
   # Include default devise modules. Others available are:
