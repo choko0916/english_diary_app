3205af7dccd9823b2e3ff33651682a393f0210dc
choko0916 <n.yuka0702@gmail.com>
2023-01-22 19:55:42 +0900

    #11 [update] 日記を新規投稿するためのアクションを追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,12 +2,29 @@ class DiariesController < ApplicationController
   def index; end
 
   def show; end
-  def new; end
+
+  def new
+    @diary = Diary.new
+  end
 
   def edit; end
-  def create; end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path, notice: t(".diary_new_notice")
+    else
+      render new_diary_path, alert: t(".diary_new_alert")
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:id, :user_id, :japanese_diary, :english_diary, :photo, :private_flag)
+  end
 end
