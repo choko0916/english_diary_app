0016b38d6c92d981b671998f3ddff60f85055b65
choko0916 <n.yuka0702@gmail.com>
2023-01-22 16:05:53 +0900

    #8 [update] ログインユーザーがアバターを登録していれば、ナビに表示させるようにした

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,9 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  process resize_to_fit: [60, 60]
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
