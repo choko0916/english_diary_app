ff60e2c775d277a288527229e18bcfe6d19bbf12
choko0916 <n.yuka0702@gmail.com>
2023-01-22 15:27:03 +0900

    #8 [fix] active_storage_validationsが用意しているvalidateであるcontent_type、sizeの記述を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
 
-  validates :avatar, content_type: { in: %w[image/jpeg image/gif image/png],
-                                     message: "有効なフォーマットではありません" },
-                     size: { less_than: 5.megabytes, message: " 5MBを超える画像はアップロードできません" }
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
