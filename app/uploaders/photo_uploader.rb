6715859f82bcf24a8ab5e8d5b589692dd0c3647a
choko0916 <n.yuka0702@gmail.com>
2023-01-24 02:24:34 +0900

    #21 [update] 画像を縦横比を維持したまま、縦横を100×100以内にリサイズするようにした

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -30,7 +30,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fit: [100, 100]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
