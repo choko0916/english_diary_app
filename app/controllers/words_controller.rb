30fc956a7b294c001df008fbb2d4c6a1ec9f5695
choko0916 <n.yuka0702@gmail.com>
2023-01-25 16:11:40 +0900

    #14 [add] wordsコントローラーを作成

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,13 @@
+class WordsController < ApplicationController
+  def index; end
+
+  def show; end
+  def new; end
+
+  def edit; end
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
