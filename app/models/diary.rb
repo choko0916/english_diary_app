28749e615712ee6530b75587a347289857f344cb
choko0916 <n.yuka0702@gmail.com>
2023-01-26 18:41:30 +0900

    #14 [update] 親モデル（Diaryモデル）を保存するときに、Associationで関連づけた子モデル（Wordモデル）も一緒に保存することができるメソッドを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,7 @@
 class Diary < ApplicationRecord
   belongs_to :user
   has_many :words, dependent: :destroy
+  accepts_nested_attributes_for :words, allow_destroy: true
   validates :english_diary, presence: true
   mount_uploader :photo, PhotoUploader
 end
