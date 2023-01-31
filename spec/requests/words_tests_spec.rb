e8cbe9aaa0106f37aee8111a29b0c4813e18b828
choko0916 <n.yuka0702@gmail.com>
2023-02-01 08:00:53 +0900

    #15 [fix] rubocopの指摘により、シングルクオートをダブルクオートに修正

--- spec/requests/words_tests_spec.rb
+++ spec/requests/words_tests_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "WordsTests", type: :request do
   describe "GET /test" do
@@ -7,5 +7,4 @@ RSpec.describe "WordsTests", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
