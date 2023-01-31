6c322305bb458aa93f1302fd1ec040027eda1b7b
choko0916 <n.yuka0702@gmail.com>
2023-02-01 08:27:10 +0900

    #15 [update] WordモデルとExamQuestionモデルに１対多の関係を持たせた

--- app/models/exam_question.rb
+++ app/models/exam_question.rb
@@ -1,3 +1,4 @@
 class ExamQuestion < ApplicationRecord
   belongs_to :exam
+  belongs_to :word
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,5 @@
 class Word < ApplicationRecord
   belongs_to :user
   belongs_to :diary
+  has_many :exam_questions, dependent: :destroy
 end
