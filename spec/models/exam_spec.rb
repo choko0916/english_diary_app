caac7a6110100ee2a3a8f95179662d91be6c8e7f
choko0916 <n.yuka0702@gmail.com>
2023-02-01 08:03:28 +0900

    #15 [fix] rubocopの指摘により、不要なspec typeの記載を削除

--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe Exam, type: :model do
+RSpec.describe Exam do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/words_tests_spec.rb
+++ spec/requests/words_tests_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "WordsTests", type: :request do
+RSpec.describe "WordsTests" do
   describe "GET /test" do
     it "returns http success" do
       get "/words_tests/test"
