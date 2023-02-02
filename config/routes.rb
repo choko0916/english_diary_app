64a9319d793410e2888165e8f8189e0a6b4cce12
choko0916 <n.yuka0702@gmail.com>
2023-02-02 20:23:25 +0900

    #15 [update] examsコントローラー各アクションのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :diaries
   resources :all_diaries, only: [:index]
   resources :words
-  resources :exams, only: [:show, :create]
+  resources :exams, only: [:show, :new, :create]
   resources :exam_questions, only: [:show]
   get "words_tests/test"
   devise_for :users, controllers: {
