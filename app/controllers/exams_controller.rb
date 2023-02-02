308941633a2c78203b9ada03337d3e6f16321c58
choko0916 <n.yuka0702@gmail.com>
2023-02-02 23:01:06 +0900

    #15 [update] 単語テストで出題する単語を、ユーザーが登録した単語リストからランダムに取得し、exam_question_pathに渡すようにした

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -4,7 +4,13 @@ class ExamsController < ApplicationController
   end
 
   def create
-    exam = Exam.new(user: current_user)
-    exam.save!
+    @exam = Exam.new(user: current_user)
+    if @exam.save!
+      @new_question = ExamQuestion.new(word_id: current_user.words.sample.id, exam_id: @exam.id)
+      @new_question.save!
+      redirect_to exam_question_path(id: @new_question.id)
+    else
+      render :new
+    end
   end
 end
