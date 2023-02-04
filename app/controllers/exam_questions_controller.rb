b2f6c0d84e028b0198546a4ea4df9eab311ff707
choko0916 <n.yuka0702@gmail.com>
2023-02-04 21:32:03 +0900

    #15 [update] 日本語の英語訳を入力後、正しいかどうか判断し答えをis_correctに記録後、次の単語クイズを生成するupdateアクションを作成

--- app/controllers/exam_questions_controller.rb
+++ app/controllers/exam_questions_controller.rb
@@ -2,4 +2,12 @@ class ExamQuestionsController < ApplicationController
   def show
     @exam_question = ExamQuestion.find(params[:id])
   end
+
+  def update
+    @exam_question = ExamQuestion.find(params[:id])
+    @exam_question.update!(is_correct: @exam_question.word.english_word == params[:english_word])
+    @exam = Exam.find(@exam_question.exam_id)
+    @new_question = ExamQuestion.create!(word_id: current_user.words.sample.id, exam_id: @exam.id)
+    redirect_to exam_question_path(id: @new_question.id)
+  end
 end
