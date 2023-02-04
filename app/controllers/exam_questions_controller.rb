cc3010335009c5133bb1a53ce5a7da70b29abad4
choko0916 <n.yuka0702@gmail.com>
2023-02-04 22:58:21 +0900

    #15 [fix]　正しくparamsを受け取ることが出来るよう修正

--- app/controllers/exam_questions_controller.rb
+++ app/controllers/exam_questions_controller.rb
@@ -5,7 +5,7 @@ class ExamQuestionsController < ApplicationController
 
   def update
     @exam_question = ExamQuestion.find(params[:id])
-    @exam_question.update!(is_correct: @exam_question.word.english_word == params[:english_word])
+    @exam_question.update!(is_correct: @exam_question.word.english_word == params[:exam_question][:english_word])
     @exam = Exam.find(@exam_question.exam_id)
     @new_question = ExamQuestion.create!(word_id: current_user.words.sample.id, exam_id: @exam.id)
     redirect_to exam_question_path(id: @new_question.id)
