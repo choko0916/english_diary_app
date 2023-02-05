5e584139a43d4f12da37ae1da78bb7575bc421ab
choko0916 <n.yuka0702@gmail.com>
2023-02-06 01:19:20 +0900

    #15 [update] 単語テストを10問解答すると、テスト開始ページに戻るようにした

--- app/controllers/exam_questions_controller.rb
+++ app/controllers/exam_questions_controller.rb
@@ -7,7 +7,11 @@ class ExamQuestionsController < ApplicationController
     @exam_question = ExamQuestion.find(params[:id])
     @exam_question.update!(is_correct: @exam_question.word.english_word == params[:exam_question][:english_word])
     @exam = Exam.find(@exam_question.exam_id)
-    @new_question = ExamQuestion.create!(word_id: current_user.words.sample.id, exam_id: @exam.id)
-    redirect_to exam_question_path(id: @new_question.id)
+    if @exam.exam_questions.size == 10
+      redirect_to new_exam_path
+    else
+      @new_question = ExamQuestion.create!(word_id: current_user.words.sample.id, exam_id: @exam.id)
+      redirect_to exam_question_path(id: @new_question.id)
+    end
   end
 end
