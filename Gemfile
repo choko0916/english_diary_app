7aa27d5ede7b3bb4c234fd4476a23d83fe2bb844
choko0916 <n.yuka0702@gmail.com>
2023-02-08 20:40:53 +0900

    #13 [update] Hotwireを導入し、turbo_confirmメッセージが表示されるようにした

--- Gemfile
+++ Gemfile
@@ -93,3 +93,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "hotwire-rails", "~> 0.1.3"
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,7 @@
 pin "application", preload: true
 pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
