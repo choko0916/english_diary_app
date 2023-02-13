682ec1939ef237180d127934eb3b03ccbc02b7c4
choko0916 <n.yuka0702@gmail.com>
2023-02-13 17:01:44 +0900

    #20 [update] ハンバーガーメニュー完成

--- app/javascript/custom/layout.js
+++ app/javascript/custom/layout.js
@@ -3,8 +3,15 @@
 {
   const open = document.getElementById("open");
   const overlay = document.querySelector(".overlay");
+  const close = document.getElementById("close");
   
   open.addEventListener("click", () => {
     overlay.classList.add("show");
+    open.classList.add("hide");
+  })
+
+  close.addEventListener("click", () => {
+    overlay.classList.remove("show");
+    open.classList.remove("hide");
   });
 }
