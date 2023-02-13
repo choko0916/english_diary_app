8f1f489050b86920d0cce49e44ea1818ba04ab37
choko0916 <n.yuka0702@gmail.com>
2023-02-14 04:15:11 +0900

    #43 [update] 単語登録時は、「単語、フレーズ（日本語）」、「単語、フレーズ（英語）」にvalidationをかけて入力必須にした

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,6 @@ class Word < ApplicationRecord
   belongs_to :user
   belongs_to :diary
   has_many :exam_questions, dependent: :destroy
+  validates :japanese_word, presence: true
+  validates :english_word, presence: true
 end
