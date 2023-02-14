ab15beb16cabf952ba33993317d2f3f0ccca78da
choko0916 <n.yuka0702@gmail.com>
2023-02-15 03:25:18 +0900

    #48 [update] Rspecのテストコード中でFactoryBotのメソッドを使用する際に、名前空間を指定しなくてすむようにした

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include FactoryBot::Syntax::Methods
+
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
