dd85473ae15696bf1919b8449949e6b42ff17aea
choko0916 <n.yuka0702@gmail.com>
2023-02-17 06:23:21 +0900

    #48 [add] 日記投稿ページのsystemsシステムスペックを作成

---
+++ spec/system/diaries_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "日記投稿ページ" do
+  let!(:english_diary) { create(:english_diary) }
+
+  before do
+    visit new_diary_path
+  end
+
+  it "必要項目（「日記（英語）」）を入力し、登録ボタンをクリックすると、ユーザーの日記一覧ページに移動すること" do
+    fill_in "日記（英語）", with: english_diary
+    click_on "登録"
+    expect(page).to have_current_path diaries_path, ignore_query: true
+  end
+
+  # it "英語日記を入力せずに、登録ボタンをクリックすると、エラーメッセージが表示され、ページ遷移しないこと" do
+  # end
+
+  # it "「単語、フレーズを追加する」ボタンを押すと、日本語・英語の単語、フレーズ入力欄、メモ入力欄がボタンを押した回数だけ表示されること" do
+  # end
+
+  # it "「英語日記」、「日本語の単語・フレーズ」、「英語の単語・フレーズ」を入力し、登録ボタンを押すと、登録に成功し、ユーザーの日記一覧ページに遷移すること" do
+  # end
+
+  # it "「日本語の単語・フレーズ」または「英語の単語・フレーズ」を入力せずに、登録ボタンをクリックすると、エラーメッセージが表示され、ページ遷移しないこと" do
+  # end
+end
