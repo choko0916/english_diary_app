feae403b9d2cca9d4c7e974c5f4eb6230c9a7261
choko0916 <n.yuka0702@gmail.com>
2023-02-18 02:33:42 +0900

    #48 [update] diaryのテストデータにprivate_flagカラムを追加

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     japanese_diary { Faker::Quote.singular_siegler }
     english_diary { Faker::Quote.famous_last_words }
     photo { Faker::Avatar.image }
+    private_flag { Faker::Boolean.boolean }
     created_at { Faker::Date }
   end
 end
