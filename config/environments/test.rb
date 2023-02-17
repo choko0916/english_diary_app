99037b8ac7cce2610867060f52ace962a05da1b7
choko0916 <n.yuka0702@gmail.com>
2023-02-18 02:17:21 +0900

    #48 [fix] 必要な空欄を追加

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  config.action_mailer.default_url_options = { host: "samplehost"}
+  config.action_mailer.default_url_options = { host: "samplehost" }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
