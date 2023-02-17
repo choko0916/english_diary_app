c2b6498a956e6328e6f0363e556bb237cbe5915d
choko0916 <n.yuka0702@gmail.com>
2023-02-18 01:09:32 +0900

    #48 [fix] RSpecでのエラーのため、記述をtest環境のファイルに移した

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  d
+  config.action_mailer.default_url_options = { host: "samplehost"}
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
