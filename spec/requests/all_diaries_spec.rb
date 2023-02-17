545878943ba1d1ec66494ec987bc5741ff596ed4
choko0916 <n.yuka0702@gmail.com>
2023-02-18 00:41:01 +0900

    #48 [update] 「公開の日記が表示されていること」、「非公開の日記が公開されていないこと」を確かめるテストを追加

--- spec/requests/all_diaries_spec.rb
+++ spec/requests/all_diaries_spec.rb
@@ -2,6 +2,23 @@ require "rails_helper"
 
 RSpec.describe "AllDiaries" do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:open_diary) { create(:diary, private_flag: false) }
+    let(:private_diary) { create(:diary, private_flag: true) }
+
+    before do
+      get all_diaries_path
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "公開の日記が表示されていること" do
+      expect(response.body).to include(private_diary)
+    end
+
+    it "非公開の日記が公開されていないこと" do
+      expect(response.body).not_to include(private_diary)
+    end
   end
 end
