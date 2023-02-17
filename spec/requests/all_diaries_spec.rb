76031cd933c62665bd98ec98523507d9288d7772
choko0916 <n.yuka0702@gmail.com>
2023-02-18 01:43:33 +0900

    #48 [update] create_listを使用し、公開している日記と、非公開の日記のテストデータを４つずつ作成するようにした

--- spec/requests/all_diaries_spec.rb
+++ spec/requests/all_diaries_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe "AllDiaries" do
   describe "GET /index" do
-    let(:open_diary) { create(:diary, private_flag: false) }
-    let(:private_diary) { create(:diary, private_flag: true) }
+    let(:open_diaries) { create_list(:diary, 4, private_flag: false) }
+    let(:private_diaries) { create_list(:diary, 4, private_flag: true) }
 
     before do
       get all_diaries_path
@@ -14,11 +14,11 @@ RSpec.describe "AllDiaries" do
     end
 
     it "公開の日記が表示されていること" do
-      expect(response.body).to include(private_diary)
+      expect(response.body).to include(private_diaries)
     end
 
     it "非公開の日記が公開されていないこと" do
-      expect(response.body).not_to include(private_diary)
+      expect(response.body).not_to include(private_diaries)
     end
   end
 end
