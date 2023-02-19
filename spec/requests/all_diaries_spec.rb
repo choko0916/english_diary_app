dece2941885d51229906fbfd54bb3a151dd66681
choko0916 <n.yuka0702@gmail.com>
2023-02-19 15:11:38 +0900

    #48 [fix] 「公開の日記が表示されていること」、「非公開の日記が公開されていないこと」を正しくテストするようにコードを修正

--- spec/requests/all_diaries_spec.rb
+++ spec/requests/all_diaries_spec.rb
@@ -3,27 +3,27 @@ require "rails_helper"
 RSpec.describe "AllDiaries" do
   describe "GET /index" do
     let!(:open_diaries) do
-      %w[10 20 30 40].map do |created_at|
+      %w[10 20].map do |created_at|
         create(:diary, private_flag: false, created_at:)
       end
     end
-    let(:private_diaries) { create_list(:diary, 4, private_flag: true) }
+    let(:private_diaries) { create_list(:diary, 2, private_flag: true) }
 
     before do
       get all_diaries_path
     end
 
-    it "returns http success" do
-      binding.pry
-      expect(response).to have_http_status(:success)
+    it "returns http success"       expect(response).to have_http_status(:success)
     end
 
-    it "公開の日記が作成日の降順で表示されていること" do
-      expect(response.body).to include(private_diaries)
+    it "公開の日記が表示されていること" do
+      expect(response.body).to include(open_diaries.first.english_diary)
+      expect(response.body).to include(open_diaries.second.english_diary)
     end
 
     it "非公開の日記が公開されていないこと" do
-      expect(response.body).not_to include(private_diaries)
+      expect(response.body).not_to include(private_diaries.first.english_diary)
+      expect(response.body).not_to include(private_diaries.second.english_diary)
     end
   end
 end
