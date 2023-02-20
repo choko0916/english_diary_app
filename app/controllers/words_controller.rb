266402ac14ed59d427181739816dd1851cb4a986
choko0916 <n.yuka0702@gmail.com>
2023-02-20 13:45:54 +0900

    #48 [update] 使用していないアクションを削除

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,12 +2,4 @@ class WordsController < ApplicationController
   def index
     @words = Word.where(user_id: current_user.id).order(created_at: "DESC")
   end
-
-  def show; end
-  def new; end
-
-  def edit; end
-  def create; end
-
-  def update; end
 end
