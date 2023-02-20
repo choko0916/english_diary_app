92fb93be0c5c1b4f0fb27788a21f857e086e099b
choko0916 <n.yuka0702@gmail.com>
2023-02-20 21:09:22 +0900

    #48 [update] 使用するFakerのデータを「’」が入っていないものに変更

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :diary do
     association :user
-    japanese_diary { Faker::Emotion.unique.adjective }
-    english_diary { Faker::Coffee.unique.blend_name }
+    japanese_diary { Faker::Creature::Animal.unique.name }
+    english_diary { Faker::TvShows::Friends.unique.character }
     photo { Faker::Avatar.unique.image }
     private_flag { Faker::Boolean.boolean }
     created_at { Faker::Date.unique.in_date_period }
