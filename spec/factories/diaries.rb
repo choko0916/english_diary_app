a2227034e08682953d751a46540bcbe90b851377
choko0916 <n.yuka0702@gmail.com>
2023-02-20 13:04:49 +0900

    #48 [update] データに「'」が入っていると記号とみなされてしまい、テストが通らないことがあるので、「'」が入っていないデータを作成

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :diary do
     association :user
     japanese_diary { Faker::Quote.singular_siegler }
-    english_diary { Faker::Quote.famous_last_words }
+    english_diary { Faker::Coffee.unique.blend_name }
     photo { Faker::Avatar.image }
     private_flag { Faker::Boolean.boolean }
     created_at { Faker::Date.in_date_period }
