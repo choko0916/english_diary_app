b71deeb800c893c5a463dc774dfccd6ee2a41cc2
choko0916 <n.yuka0702@gmail.com>
2023-02-23 18:13:28 +0900

    #48 [update] Emotion.nounの数が少ないためQuote.robinに変更

--- spec/factories/word.rb
+++ spec/factories/word.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :word do
     association :user
     association :diary
-    japanese_word { Faker::Emotion.unique.noun }
+    japanese_word { Faker::Quote.unique.robin }
     english_word { Faker::Hobby.unique.activity }
   end
 end
