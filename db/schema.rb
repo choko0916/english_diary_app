8b58298723a7884493f2664c20d83997bbf01aa8
choko0916 <n.yuka0702@gmail.com>
2023-02-23 18:05:26 +0900

    #48 [update] かぶっていたusernameを削除

--- db/schema.rb
+++ db/schema.rb
@@ -86,9 +86,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_230915) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "bio"
+    t.string "username", limit: 50, default: "名無し", null: false
     t.string "avatar"
-    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
