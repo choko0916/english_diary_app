5f9c0878b9308d20200dcbd9a5bff85044376c97
choko0916 <n.yuka0702@gmail.com>
2023-02-25 20:13:34 +0900

    #48 [fix] 検証したいものが検証できるようにテストを修正

--- spec/requests/exam_questions_spec.rb
+++ spec/requests/exam_questions_spec.rb
@@ -3,8 +3,11 @@ require "rails_helper"
 RSpec.describe "ExamQuestions" do
   let!(:login_user) { create(:user) }
   let!(:exam1) { create(:exam, user: login_user) }
-  let!(:word1) { create(:word, english_word: "apple", user: login_user) }
-  let!(:exam_question1) { create(:exam_question, exam: exam1, word: word1, is_correct: nil) }
+  let!(:exam_question1) { create(:exam_question, exam: exam1) }
+
+  before do
+    sign_in login_user
+  end
 
   describe "GET /show" do
     it "returns http success" do
@@ -14,19 +17,10 @@ RSpec.describe "ExamQuestions" do
   end
 
   describe "POST /update" do
-    context "単語テストでの回答が正解の場合" do
-      it "is_correctカラムをtrueで上書きすること" do
-        patch exam_question_path(exam_question1.id), params: { exam_question: { is_correct: "apple" } }
-        expect(exam_question1.is_correct).to be true
-      end
-    end
-
-    context "単語テストでの回答が不正解の場合" do
-      it "is_correctカラムをfalseで上書きすること" do
-        patch exam_question_path(exam_question1.id), params: { exam_question: { is_correct: "wrong_answer" } }
-        expect(exam_question1.is_correct).to be false
-      end
-    end
+    let!(:exam3) { create(:exam, exam_questions: less_than_ten_exam_questions) }
+    let!(:less_than_ten_exam_questions) { create_list(:exam_question, 8, word: sample, is_correct: nil) }
+    let!(:sample) { login_user_words.sample }
+    let!(:login_user_words) { create_list(:word, 10, user: login_user) }
 
     context "単語テスト10問目が終了した場合" do
       let!(:ten_exam_questions) { create_list(:exam_question, 10) }
@@ -40,14 +34,26 @@ RSpec.describe "ExamQuestions" do
     end
 
     context "単語テストが10問目以下の場合" do
-      let!(:less_than_ten_exam_questions) { create_list(:exam_question, 8) }
-      let!(:exam3) { create(:exam, exam_questions: less_than_ten_exam_questions) }
-
       it "exam_questionsテーブルに新しいデータが１件登録されること" do
         expect do
           patch exam_question_path(exam3.exam_questions.last.id),
                 params: { exam_question: { english_word: exam3.exam_questions.last.word.english_word } }
         end.to change(ExamQuestion, :count).by(1)
+      end
+    end
+
+    context "単語テストでの回答が正解であれば" do
+      it "is_correctカラムをtrueで上書きすること" do
+        patch exam_question_path(exam3.exam_questions.last.id), params: { exam_question: { english_word: sample.english_word } }
+        binding.pry
+        expect(exam3.exam_questions.last.reload.is_correct).to be true
+      end
+    end
+
+    context "単語テストでの回答が不正解であれば" do
+      it "is_correctカラムをfalseで上書きすること" do
+        patch exam_question_path(exam3.exam_questions.last.id), params: { exam_question: { english_word: "wrong_answer" } }
+        expect(exam3.exam_questions.last.reload.is_correct).to be false
       end
     end
   end
