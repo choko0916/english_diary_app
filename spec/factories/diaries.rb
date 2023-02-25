8e3e42422c0416cd569142b424d57206485e456f
choko0916 <n.yuka0702@gmail.com>
2023-02-25 20:15:14 +0900

    #48 [update] テストデータ変更

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :diary do
     association :user
-    japanese_diary { Faker::Creature::Animal.unique.name }
-    english_diary { Faker::TvShows::Friends.unique.character }
+    japanese_diary { Faker::Book.unique.author }
+    english_diary { Faker::Book.unique.title }
     photo { Faker::Avatar.unique.image }
     private_flag { Faker::Boolean.boolean }
     created_at { Faker::Date.unique.in_date_period }
