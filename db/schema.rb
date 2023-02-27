787f4ebf9c7767cecfb7982218a1574694c82405
choko0916 <n.yuka0702@gmail.com>
2023-02-28 02:00:55 +0900

    #18 [update] usersテーブルにbioカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_230915) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_165837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_230915) do
     t.datetime "updated_at", null: false
     t.string "username", limit: 50, default: "名無し", null: false
     t.string "avatar"
+    t.text "bio"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
