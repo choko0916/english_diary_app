33cd7e461bd21d09c352d66f7dc826c0054b922c
choko0916 <n.yuka0702@gmail.com>
2023-02-28 16:23:47 +0900

    #57 [fix] rubocopでの指摘を反映

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,11 +72,11 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :truncation
   end
 
-  config.before(:each) do
+  config.before do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.after do
     DatabaseCleaner.clean
   end
 
